03ea8ddef7ada0b98d7d33d87ea7f865088cbd2a
Mike Christianson <mike.christianson@meltmedia.com>
2014-08-06 15:32:15 -0700

    cleanup before and after

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 #!/bin/sh -ex
+rm -i vagrant-vbox-trusty-docker.box
+vagrant destroy
 vagrant up
 vagrant package --base vagrant-vbox-trusty-docker --output vagrant-vbox-trusty-docker.box
+vagrant halt
